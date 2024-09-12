99176374dc5e22175007170c3d8a61edd8287c63
Thomas Dupre <thomasdpr-pro@outlook.com>
2024-09-12 05:43:34 +0200

    Exercise 5 completed

---
+++ Exercice05/main.py
@@ -0,0 +1,28 @@
+def sum(a, b):
+    """
+    Calcule la somme de deux nombres.
+
+    Args:
+        a (int | float): le premier nombre
+        b (int | float): le second nombre
+
+    Returns:
+        int | float: la somme des deux nombres
+    """
+
+    return a + b
+
+
+def subtraction(a, b):
+    """
+    Calcule la différence de deux nombres.
+
+    Args:
+        a (int | float): le premier nombre
+        b (int | float): le second nombre
+
+    Returns:
+        int | float: la différence des deux nombres
+    """
+
+    return a - b
